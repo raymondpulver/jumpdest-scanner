f2b516fdb9a154aa3aebeec0e35d7a7ca14c71bb
Raymond Pulver IV <raymond.pulver@my.uri.edu>
2020-05-31 02:36:46 +0000

    fix missing OR

--- contracts/BitsLib.sol
+++ contracts/BitsLib.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.6.0;
 library BitsLib {
   function setBit(uint256 ptrStart, uint256 bitPosition) internal pure {
     assembly {
-      mstore8(add(ptrStart, div(bitPosition, 0x8)), shr(and(0x7, bitPosition), 0x80))
+      let bytePosition := div(bitPosition, 0x8)
+      let ptr := add(ptrStart, bytePosition)
+      mstore8(ptr, or(mload(sub(ptr, 0x1f)), shr(and(0x7, bitPosition), 0x80)))
     }
   }
 }
